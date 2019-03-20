b3ec10776bc38f7c5c8464830d04a6e8c074d4d6
arizoide <ari.thomazini@gmail.com>
2019-03-20 20:06:18 -0300

    Add files via upload

---
+++ adiciona_produto.php
@@ -0,0 +1,7 @@
+<html>
+	<h1>Adicionar Produto</h1>
+	<?php
+		$nome = $_GET["nome"];		
+	?>
+	O produto <?php echo $nome?> foi adicionado.
+</html>
---
+++ formulario_produto.php
@@ -0,0 +1,8 @@
+<html>
+	<form action="adiciona_produto.php">
+		<input type="text" name="nome" /> </br>
+		<input type="text" name="valor"/> </br>
+
+		<input type="submit" value="Cadastrar"/>
+	</form>
+</html>
